f8ba8ed9de89aa9d47c91b3f1942edb2ed499846
chengzhen <wogong38@gmail.com>
2012-10-18 09:44:14 +0800

    Automated commit on 10/18/2012 Thu  09:44 AM

--- sync.bat
+++ sync.bat
@@ -11,7 +11,6 @@ for /f "delims=" %%a in ('time/t') do @set mytime=%%a
 set fvar=%mydate%%mytime% 
 
 :: add all new files 
-call "c:\Users\chengzhen\AppData\Local\GitHub\PortableGit_8810fd5c2c79c73adcc73fd0825f3b32fdb816e7\git-cmd.bat"
 
 call git add . 
 call git commit -a -m "Automated commit on %fvar%"
